e90a97c858426076d06c00db8ca93e37dc742be4
SIAE-DGAE <fa9567796@gmail.com>
2023-01-04 20:40:05 -0600

    Este sitio solo es una prueba
    No entren, trabajo de clase

---
+++ Menu/datos.js
@@ -0,0 +1,16 @@
+
+
+
+
+        function VerificarDatos() {
+            if (document.getElementById("Usuario").value == "321294878" &&
+                document.getElementById("NIP").value == "lucas222005as" &&
+                document.getElementById("captcha").value == "11") {
+                response.sendRedirect("SIAE PAGINA DE CONTROL.HTML");
+            }
+        }
+
+
+
+
+
